78d2f94d739275e5ac57e44fcc6e80064e0074ae
peter <peter>
2002-03-16 23:33:15 +0000

    Forgotten files Added Files:  	ipv6logconvhelp.h ipv6logconvoptions.h

---
+++ ipv6logconvoptions.h
@@ -0,0 +1,41 @@
+/*
+ * Project    : ipv6calc
+ * File       : ipv6logconvoptions.h
+ * Version    : $Id: ipv6logconvoptions.h,v 1.1 2002/03/16 23:33:15 peter Exp $
+ * Copyright  : 2002 by Peter Bieringer <pb (at) bieringer.de>
+ *
+ * Information:
+ *  Header file containing optios for ipvlogconv.c
+ */ 
+
+#include <getopt.h> 
+
+#include "ipv6calctypes.h"
+
+
+#ifndef _ipv6logconvoptions_h_
+
+#define _ipv6logconvoptions_h_
+
+/* Options */
+
+/* define short options */
+static char *ipv6logconv_shortopts = "vh?d:";
+
+/* define long options */
+static struct option ipv6logconv_longopts[] = {
+	{"version", 0, 0, (int) 'v'},
+	{"debug", 1, 0, (int) 'd'},
+
+	/* help options */
+	{"help", 0, 0, (int) 'h'},
+	{"examples"     , 0, 0, CMD_printexamples },
+	{"printexamples", 0, 0, CMD_printexamples },
+
+	/* options */
+	{ "out"       , 1, 0, CMD_outputtype },
+
+	{NULL, 0, 0, 0}
+};                
+
+#endif
