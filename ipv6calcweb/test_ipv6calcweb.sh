3ea8df69d61d911dab122824807dcd39bcb29086
peter <peter>
2006-06-08 10:11:31 +0000

    Add output type selection to query string, extend test program

--- ipv6calcweb/test_ipv6calcweb.sh
+++ ipv6calcweb/test_ipv6calcweb.sh
@@ -5,6 +5,11 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+if [ ipv6calcweb.cgi.in -nt ipv6calcweb.cgi ]; then
+	cat ipv6calcweb.cgi.in | sed s/\@IPV6CALC_VERSION\@/test/ >ipv6calcweb.cgi
+	chmod u+x ipv6calcweb.cgi
+fi
+
 REMOTE_ADDR="$1"
 REMOTE_HOST="client.domain.example"
 HTTP_USER_AGENT="test_ipv6calcweb"
@@ -12,6 +17,8 @@ HTTP_USER_AGENT="test_ipv6calcweb"
 SERVER_ADDR="192.0.2.1"
 SERVER_NAME="server.domain.example"
 
-export REMOTE_ADDR REMOTE_HOST HTTP_USER_AGENT SERVER_ADDR SERVER_NAME
+QUERY_STRING="$2"
+
+export REMOTE_ADDR REMOTE_HOST HTTP_USER_AGENT SERVER_ADDR SERVER_NAME QUERY_STRING
 
 ./ipv6calcweb.cgi
