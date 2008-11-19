c844dc870517c95245c33b1c21b90cdef3eab937
peter <peter>
2008-11-19 19:04:21 +0000

    Update copyright year

--- ipv6calc/ipv6calc.h
+++ ipv6calc/ipv6calc.h
@@ -1,8 +1,8 @@
 /*
  * Project    : ipv6calc
  * File       : ipv6calc.h
- * Version    : $Id: ipv6calc.h,v 1.7 2007/03/03 11:57:29 peter Exp $
- * Copyright  : 2001-2007 by Peter Bieringer <pb (at) bieringer.de>
+ * Version    : $Id: ipv6calc.h,v 1.8 2008/11/19 19:04:21 peter Exp $
+ * Copyright  : 2001-2008 by Peter Bieringer <pb (at) bieringer.de>
  *
  * Information:
  *  Main header file
@@ -15,6 +15,6 @@
 
 /* global program related definitions */
 #define PROGRAM_NAME "ipv6calc"
-#define PROGRAM_COPYRIGHT "(P) & (C) 2001-2007 by Peter Bieringer <pb (at) bieringer.de>"
+#define PROGRAM_COPYRIGHT "(P) & (C) 2001-2008 by Peter Bieringer <pb (at) bieringer.de>"
 
 #endif
