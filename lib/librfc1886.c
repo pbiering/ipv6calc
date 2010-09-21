7fd6153ce545ca780a0587056b804dccd0fd6395
peter <peter>
2010-09-21 19:01:15 +0000

    update databases         lib/librfc1886.c: take care of prefix length before printing nibbles          (bugfix, but breaking old behavior, if prefix length is used)         tag version 0.80.0

--- lib/librfc1886.c
+++ lib/librfc1886.c
@@ -1,7 +1,7 @@
 /*
  * Project    : ipv6calc
  * File       : librfc1886.c
- * Version    : $Id: librfc1886.c,v 1.13 2010/09/09 20:42:55 peter Exp $
+ * Version    : $Id: librfc1886.c,v 1.14 2010/09/21 19:01:16 peter Exp $
  * Copyright  : 2002-2010 by Peter Bieringer <pb (at) bieringer.de>
  *
  * Information:
@@ -48,11 +48,10 @@ int librfc1886_addr_to_nibblestring(ipv6calc_ipv6addr *ipv6addrp, char *resultst
 		fprintf(stderr, "%s: flag_prefixuse %d\n", DEBUG_function_name, (*ipv6addrp).flag_prefixuse);
 	};
 
-	/* 20100909: following code would take care of prefix length before printing the nibbles, but break old behavior
+	/* 20100909: take care of prefix length before printing the nibbles, but break old behavior */
 	if ((*ipv6addrp).flag_prefixuse != 0) {
 		ipv6addrstruct_maskprefix(ipv6addrp);
 	};
-	*/
 
 	if ( ((formatoptions & (FORMATOPTION_printprefix | FORMATOPTION_printsuffix | FORMATOPTION_printstart | FORMATOPTION_printend)) == 0 ) && ((*ipv6addrp).flag_prefixuse != 0) ) {
 		/* simulate old behavior */
