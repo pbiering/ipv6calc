57d4b44ffac92d8e50de1cc22f943b494ea51ab4
Peter Bieringer <pb@bieringer.de>
2019-01-26 12:06:36 +0100

    Revert "wrong bugfix"
    This reverts commit fab624da54351d1283d46f65615779d783c4103a.

--- lib/libipv6addr.c
+++ lib/libipv6addr.c
@@ -1251,7 +1251,7 @@ int addr_to_ipv6addrstruct(const char *addrstring, char *resultstring, const siz
 	addronlystring = strtok_r(tempstring, "/", ptrptr);
 	
 	if ( addronlystring == NULL ) {
-		fprintf(stderr, "Strange input: %s\n", tempstring);
+		fprintf(stderr, "Strange input: %s\n", addrstring);
 		return (1);
 	};
 
