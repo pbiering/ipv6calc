acc71b686792e11f8877b9e2dc742b2e83801340
Peter Bieringer <pb@bieringer.de>
2021-09-16 08:35:49 +0200

    display linked MD5 implementation

--- lib/ipv6calchelp.c
+++ lib/ipv6calchelp.c
@@ -2,7 +2,7 @@
  * Project    : ipv6calc
  * File       : ipv6calchelp.c
  * Version    : $Id$
- * Copyright  : 2002-2020 by Peter Bieringer <pb (at) bieringer.de>
+ * Copyright  : 2002-2021 by Peter Bieringer <pb (at) bieringer.de>
  *
  * Information:
  *  Help library
@@ -924,6 +924,14 @@ void ipv6calc_print_features_verbose(const int level_verbose) {
 #endif
 	fprintf(stderr, "\n\n");
 #endif
+
+#if defined ENABLE_OPENSSL_EVP_MD5
+	fprintf(stderr, "Linked MD5 implemenation: OpenSSL EVP MD5\n");
+#endif
+#if defined ENABLE_OPENSSL_MD5
+	fprintf(stderr, "Linked MD5 implemenation: OpenSSL legacy MD5\n");
+#endif
+
 
 	fprintf(stderr, "Compiled: %s  %s\n\n", __DATE__, __TIME__);
 
