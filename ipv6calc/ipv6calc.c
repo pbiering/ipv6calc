b1e8dda239da5ef7a40f8ca2b1281b83caa9af9b
Peter Bieringer <pb@bieringer.de>
2021-09-19 17:02:36 +0200

    flexible database selection

--- ipv6calc/ipv6calc.c
+++ ipv6calc/ipv6calc.c
@@ -143,6 +143,9 @@ int main(int argc, char *argv[]) {
 	s_iid_statistics iid_statistics;
 	int iid_random_result;
 
+	/* database source */
+	int db_source;
+
 	/* clear address structures */
 	ipv6addr_clearall(&ipv6addr);
 	ipv6addr_clearall(&ipv6addr2);
@@ -828,11 +831,13 @@ int main(int argc, char *argv[]) {
 			exit(EXIT_FAILURE);
 		};
 
-		// TODO SOURCE SELECTION
+		// TODO SOURCE SELECTION in case more databases support dump format
+		source = IPV6CALC_DB_SOURCE_EXTERNAL;
+
 		if ((filter_master.filter_ipv4addr.active == 0) && (filter_master.filter_ipv6addr.active != 0)) {
-			libipv6calc_db_dump(IPV6CALC_DB_SOURCE_EXTERNAL, IPV6CALC_PROTO_IPV6, &filter_master, formatoptions);
+			libipv6calc_db_dump(db_source, IPV6CALC_PROTO_IPV6, &filter_master, formatoptions);
 		} else if ((filter_master.filter_ipv4addr.active != 0) && (filter_master.filter_ipv6addr.active == 0)) {
-			libipv6calc_db_dump(IPV6CALC_DB_SOURCE_EXTERNAL, IPV6CALC_PROTO_IPV4, &filter_master, formatoptions);
+			libipv6calc_db_dump(db_source, IPV6CALC_PROTO_IPV4, &filter_master, formatoptions);
 		} else {
 			fprintf(stderr, "ipv6calc action 'dbdump' is not supporting combined IPv4 and IPv6 filter\n");
 			exit(EXIT_FAILURE);
