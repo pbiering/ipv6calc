af135147ba3b797769f1d3391c6ac9387dcae3d7
Peter Bieringer <pb@bieringer.de>
2025-01-29 07:00:24 +0100

    rework for Docbook 5

--- doc/generate.sh
+++ doc/generate.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 #
-# generate HTML page out of SGML
+# generate HTML page out of XML from LyX
 #
 # Project    : ipv6calc/doc
 # File       : generate.sh
 # Copyright  : 2003-2025 by Peter Bieringer <pb (at) bieringer.de>
 # License    : GNU GPL version 2
-# Requires: openjade opensp lyx docbook-utils
+# Requires: lyx xsltproc
 #
 # 20231206/PB: change from "jade" to "openjade" (anyhow a softlink since at least EL6)
 # 20231206/PB: add option to generate directly all from LyX file
 # 20231207/PB: drop support of SGML file, LyX file is now mandatory
-# 20250128/PB: switch to docbook5, SGML dropped
+# 20250128/PB: switch to docbook5, SGML dropped, HTML generator uses now xsltproc and local db2xhtml-ipv6calc.xsl
 
 #export SP_ENCODING=UTF-8
 export SP_CHARSET_FIXED=yes
@@ -94,16 +94,17 @@ validate_sgml() {
 ## Create HTML single page from SGML
 create_html_singlepage() {
 	echo "INF: Create HTML singlepage '$file_html'"
-	openjade -t sgml -i html -V nochunks -d "${file_dsl}#html" $file_sgml >$file_html
+	#openjade -t sgml -i html -V nochunks -d "${file_dsl}#html" $file_xml >$file_html
+	xsltproc -o ipv6calc.html db2xhtml-ipv6calc.xsl ipv6calc.xml
 	local retval=$?
 	if [ $retval -eq 0 ]; then
-		if head -1 $file_html | grep -q DOCTYPE; then
+		if head -2 $file_html | grep -q DOCTYPE; then
 			echo "INF: Create HTML singlepage - done"
 			return 0
 		fi
 	fi
 
-	echo "ERR: Create HTML singlepage - an error occurs!"
+	echo "ERR: Create HTML singlepage - an error occurs! (r=$retval)"
 	return $retval
 }
 
@@ -134,12 +135,32 @@ export_sgml_xml() {
 	echo "INF: Export LyX->XML"
 	$lyxBin --export docbook5 $file_lyx || return 1
 	echo "INF: Export LyX->XML successful"
+
+	echo "INF: Adjust docbook5 XML"
+
+	## remove commented docbook details
+	perl -pi -e 's#&lt;(\/?(author|firstname|surname|affiliation|address|revhistory|revision|revnumber|date|authorinitials))&gt;#<$1>#g' $file_xml
+	#grep firstname $file_xml
+	perl -pi -e 's#<!--\s*(</?(author|revision|revhistory)>)#$1#g' $file_xml
+	#grep firstname $file_xml
+	perl -pi -e 's#(</?(author|revision|revhistory)>)\s*-->#$1#g' $file_xml
+	#grep firstname $file_xml
+
+	## add surrounding <info>..</info>
+	perl -pi -e 's#(<author>)#<info>$1#g' $file_xml
+	perl -pi -e 's#(</revhistory>)#$1</info>#g' $file_xml
+
+	echo "INF: Adjust docbook5 XML successful"
+
+	#echo "INF: Convert XML->SGML"
+	#$lyxBin --export docbook $file_lyx || return 1
+	#echo "INF: Export LyX->SGML successful"
 }
 
 
 ### Main
 export_sgml_xml || exit 1
 
-validate_sgml || exit 1
+#validate_sgml || exit 1
 
 create_html_singlepage || exit 1
